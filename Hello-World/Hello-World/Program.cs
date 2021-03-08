4ee485bb1bdd7ebed3f6982d75b9ecd2a196744f
Ga-Young-Lee <yb2018675047@ks.ac.kr>
2021-03-08 22:04:17 +0900

    Hello-World

---
+++ Hello-World/Hello-World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2018675047 이가영");
+        }
+    }
+}

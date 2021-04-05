77e5356fe3f1d4e3e3522b22e7e76ba80a68d779
Ga-Young-Lee <80228478+Ga-Young-Lee@users.noreply.github.com>
2021-04-05 18:33:38 +0900

    Add files via upload

---
+++ CSharpClass/08CommandLineArgsApp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Class
+{
+    class CommandLineArgsApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; ++i)
+                Console.WriteLine("Argument[{0}] = {1}", i, args[i]);
+        }
+    }
+
+}
---
+++ CSharpClass/16PropertyWithoutFieldApp.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Class
+{
+    class PropertyWithoutField
+    {
+        public string Message
+        {
+            get { return Console.ReadLine(); }
+            set { Console.WriteLine(value); }
+        }
+    }
+    class PropertyWithoutFieldApp
+
+    {
+        public static void Main()
+        {
+            PropertyWithoutField obj = new PropertyWithoutField();
+            obj.Message = obj.Message;
+        }
+    }
+
+
+
+}

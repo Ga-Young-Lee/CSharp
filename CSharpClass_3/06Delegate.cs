2eda19ec421176fcd737684826d0f9fbebb39d87
Ga-Young-Lee <80228478+Ga-Young-Lee@users.noreply.github.com>
2021-04-12 22:50:50 +0900

    Add files via upload

---
+++ CSharpClass_3/06Delegate.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Class_3
+{
+    delegate int MyDelegate(int a, int b);
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    class Delegate
+
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3, 4));
+            Callback = new MyDelegate(Calculator.Minus);
+            Console.WriteLine(Callback(7, 5));
+        }
+    }
+}
---
+++ CSharpClass_3/12MultiCastApp.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+namespace Class_3
+{
+    delegate void MultiCastDelegate();
+    class Schedule
+    {
+        public void Now()
+        {
+            Console.WriteLine("Time : " + DateTime.Now.ToString());
+        }
+        public static void Today()
+        {
+            Console.WriteLine("Date : " + DateTime.Today.ToString());
+        }
+    }
+    class MultiCastApp
+    {
+        public static void Main()
+        {
+            Schedule obj = new Schedule();
+            MultiCastDelegate mcd = new MultiCastDelegate(obj.Now);
+            mcd += new MultiCastDelegate(Schedule.Today);
+            mcd();
+        }
+    }
+}
